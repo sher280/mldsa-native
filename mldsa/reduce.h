3e71ef362ec2e1c97e79b814f22d89d4a0dc9c2e
Jake Massimo <jakemas@amazon.com>
2025-03-12 12:27:03 -0700

    CBMC: Add spec and proof for reduce32
    Resolves https://github.com/pq-code-package/mldsa-native/issues/15
    
    Signed-off-by: Jake Massimo <jakemas@amazon.com>

--- mldsa/reduce.h
+++ mldsa/reduce.h
@@ -6,16 +6,25 @@
 #define REDUCE_H
 
 #include <stdint.h>
+#include "cbmc.h"
 #include "params.h"
 
 #define MONT -4186625  // 2^32 % Q
 #define QINV 58728449  // q^(-1) mod 2^32
+#define REDUCE_DOMAIN_MAX (INT32_MAX - (1 << 22))
+#define REDUCE_RANGE_MAX 6283009
 
 #define montgomery_reduce DILITHIUM_NAMESPACE(montgomery_reduce)
 int32_t montgomery_reduce(int64_t a);
 
 #define reduce32 DILITHIUM_NAMESPACE(reduce32)
-int32_t reduce32(int32_t a);
+int32_t reduce32(int32_t a)
+__contract__(
+  requires(a <= REDUCE_DOMAIN_MAX)
+  ensures(return_value >= -REDUCE_RANGE_MAX)
+  ensures(return_value <   REDUCE_RANGE_MAX)
+  ensures((return_value - a) % Q == 0)
+);
 
 #define caddq DILITHIUM_NAMESPACE(caddq)
 int32_t caddq(int32_t a);
