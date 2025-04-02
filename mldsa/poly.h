3c3b4e14053ea2e078c6154f777b4ec9f2a75a73
Matthias J. Kannwischer <matthias@kannwischer.eu>
2025-04-02 13:32:33 +0800

    CBMC: Add contract and proof for poly_decompose
    Signed-off-by: Matthias J. Kannwischer <matthias@kannwischer.eu>

--- mldsa/poly.h
+++ mldsa/poly.h
@@ -59,8 +59,19 @@ void poly_pointwise_montgomery(poly *c, const poly *a, const poly *b);
 
 #define poly_power2round MLD_NAMESPACE(poly_power2round)
 void poly_power2round(poly *a1, poly *a0, const poly *a);
+
 #define poly_decompose MLD_NAMESPACE(poly_decompose)
-void poly_decompose(poly *a1, poly *a0, const poly *a);
+void poly_decompose(poly *a1, poly *a0, const poly *a)
+__contract__(
+  requires(memory_no_alias(a1,  sizeof(poly)))
+  requires(memory_no_alias(a0, sizeof(poly)))
+  requires(memory_no_alias(a, sizeof(poly)))
+  requires(array_bound(a->coeffs, 0, MLDSA_N, 0, MLDSA_Q))
+  assigns(memory_slice(a1, sizeof(poly)))
+  assigns(memory_slice(a0, sizeof(poly)))
+  ensures(array_bound(a1->coeffs, 0, MLDSA_N, 0, (MLDSA_Q-1)/(2*MLDSA_GAMMA2)))
+  ensures(array_abs_bound(a0->coeffs, 0, MLDSA_N, MLDSA_GAMMA2+1))
+);
 
 #define poly_make_hint MLD_NAMESPACE(poly_make_hint)
 unsigned int poly_make_hint(poly *h, const poly *a0, const poly *a1)
---
+++ proofs/cbmc/poly_decompose/poly_decompose_harness.c
@@ -0,0 +1,11 @@
+// Copyright (c) 2025 The mldsa-native project authors
+// SPDX-License-Identifier: Apache-2.0
+
+#include "poly.h"
+
+
+void harness(void)
+{
+  poly *a0, *a1, *a;
+  poly_decompose(a1, a0, a);
+}
