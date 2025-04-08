3e9b6d8a0a54fd64f2f3827363e0c1cb0a804ab4
Rod Chapman <rodchap@amazon.com>
2025-04-08 14:53:51 +0100

    Refactor and prove type-safety of forward NTT.
    Refactors reference ntt() function into 3 functions,
    as per the pattern in mlkem-native:
      mld_ntt_butterfly_block()
      mld_ntt_layer()
      ntt()
    
    Adds contracts and proofs of these 3 functions.
    
    Adds and moves comments as appropriate.
    
    Signed-off-by: Rod Chapman <rodchap@amazon.com>

--- mldsa/ntt.h
+++ mldsa/ntt.h
@@ -6,10 +6,40 @@
 #define NTT_H
 
 #include <stdint.h>
+#include "cbmc.h"
 #include "params.h"
 
+/* Absolute exclusive upper bound for the output of the forward NTT */
+#define MLD_NTT_BOUND (9 * MLDSA_Q)
+
 #define ntt MLD_NAMESPACE(ntt)
-void ntt(int32_t a[MLDSA_N]);
+/*************************************************
+ * Name:        ntt
+ *
+ * Description: Computes number-theoretic transform (NTT) of
+ *              a polynomial in place.
+ *
+ *              The input is assumed to be in normal order and
+ *              coefficient-wise bound by MLDSA_Q in absolute value.
+ *
+ *              The output polynomial is in bitreversed order, and
+ *              coefficient-wise bound by MLD_NTT_BOUND in absolute value.
+ *
+ *              (NOTE: Sometimes the input to the NTT is actually smaller,
+ *               which gives better bounds.)
+ *
+ * Arguments:   - int32_t a[MLDSA_N]: pointer to in/output polynomial
+ *
+ * Specification: Implements [FIPS 204, Algorithm 41, NTT]
+ *
+ **************************************************/
+void ntt(int32_t a[MLDSA_N])
+__contract__(
+  requires(memory_no_alias(a, MLDSA_N * sizeof(int32_t)))
+  requires(array_abs_bound(a, 0, MLDSA_N, MLDSA_Q))
+  assigns(memory_slice(a, MLDSA_N * sizeof(int32_t)))
+  ensures(array_abs_bound(a, 0, MLDSA_N, MLD_NTT_BOUND))
+);
 
 #define invntt_tomont MLD_NAMESPACE(invntt_tomont)
 void invntt_tomont(int32_t a[MLDSA_N]);
