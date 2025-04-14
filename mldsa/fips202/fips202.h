7150b24f9851fd13dfff167bc554ecbe67f6ba68
manastasova <manastasova2017@fau.edu>
2025-04-14 21:23:30 +0000

    Add spec and proof for shake128_absorb_once
    Signed-off-by: manastasova <manastasova2017@fau.edu>

--- mldsa/fips202/fips202.h
+++ mldsa/fips202/fips202.h
@@ -35,7 +35,13 @@ void shake128_finalize(keccak_state *state);
 #define shake128_squeeze FIPS202_NAMESPACE(shake128_squeeze)
 void shake128_squeeze(uint8_t *out, size_t outlen, keccak_state *state);
 #define shake128_absorb_once FIPS202_NAMESPACE(shake128_absorb_once)
-void shake128_absorb_once(keccak_state *state, const uint8_t *in, size_t inlen);
+void shake128_absorb_once(keccak_state *state, const uint8_t *in, size_t inlen)
+__contract__(
+  requires(memory_no_alias(state, sizeof(keccak_state)))
+  requires(memory_no_alias(in, inlen))
+  assigns(memory_slice(state, sizeof(keccak_state)))
+);
+
 #define shake128_squeezeblocks FIPS202_NAMESPACE(shake128_squeezeblocks)
 void shake128_squeezeblocks(uint8_t *out, size_t nblocks, keccak_state *state);
 
