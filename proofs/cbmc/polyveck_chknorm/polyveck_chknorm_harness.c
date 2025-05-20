9ab2269e2b76533ab3d448c7201a5d040db37d64
Rod Chapman <rodchap@amazon.com>
2025-05-20 21:25:17 +0100

    Add contracts and proof files for polyveck_chknorm()
    Address comments arising from review of PR#260.
    
    Signed-off-by: Rod Chapman <rodchap@amazon.com>

---
+++ proofs/cbmc/polyveck_chknorm/polyveck_chknorm_harness.c
@@ -0,0 +1,12 @@
+// Copyright (c) The mldsa-native project authors
+// SPDX-License-Identifier: Apache-2.0 OR ISC OR MIT
+
+#include "polyvec.h"
+
+void harness(void)
+{
+  int32_t b;
+  polyveck *v;
+  int r;
+  r = polyveck_chknorm(v, b);
+}
