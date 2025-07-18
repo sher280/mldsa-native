f30c5d87949553ed18c2615f78ccb715989fbaa1
Rod Chapman <rodchap@amazon.com>
2025-07-18 09:01:12 +0100

    Add contracts and proofs for toplevel signature functions
      crypto_sign_signature()
      crypto_sign_signature_extmu()
      crypto_sign()
    
    Also adjust the Makefile_params.common file
    for CBMC to define the MLD_NAMESPACETOP macro
    for crypto_sign()
    
    Signed-off-by: Rod Chapman <rodchap@amazon.com>

---
+++ proofs/cbmc/crypto_sign_signature_extmu/crypto_sign_signature_extmu_harness.c
@@ -0,0 +1,15 @@
+// Copyright (c) The mldsa-native project authors
+// SPDX-License-Identifier: Apache-2.0 OR ISC OR MIT
+
+#include "sign.h"
+
+void harness(void)
+{
+  uint8_t *sig;
+  size_t *siglen;
+  uint8_t *mu;
+  uint8_t *sk;
+  int r;
+
+  r = crypto_sign_signature_extmu(sig, siglen, mu, sk);
+}
