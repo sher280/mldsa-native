93df34f481060336b34305595a9bbc42145b561a
Matthias J. Kannwischer <matthias@kannwischer.eu>
2025-07-28 13:24:30 +0800

    Add CBMC proofs for constant-time functions and adjust CBMC proofs of callers
    This adds proofs for all constant-time functions:
    - mld_ct_abs_i32
    - mld_ct_cmask_neg_i32
    - mld_ct_get_optblocker_i64
    - mld_ct_get_optblocker_u32
    - mld_ct_sel_int32
    - mld_value_barrier_i64
    - mld_value_barrier_u32
    
    The proofs for the functions using these are adjusted:
    
    - caddq
    - decompose
    - poly_chknorm
    
    Signed-off-by: Matthias J. Kannwischer <matthias@kannwischer.eu>

--- mldsa/ct.h
+++ mldsa/ct.h
@@ -36,18 +36,16 @@ extern volatile uint64_t mld_ct_opt_blocker_u64;
 static MLD_INLINE uint64_t mld_ct_get_optblocker_u64(void)
 __contract__(ensures(return_value == 0)) { return mld_ct_opt_blocker_u64; }
 
-/* TODO: proof */
 static MLD_INLINE int64_t mld_ct_get_optblocker_i64(void)
 __contract__(ensures(return_value == 0)) { return (int64_t)mld_ct_get_optblocker_u64(); }
-/* TODO: proof */
+
 static MLD_INLINE uint32_t mld_ct_get_optblocker_u32(void)
 __contract__(ensures(return_value == 0)) { return (uint32_t)mld_ct_get_optblocker_u64(); }
 
 /* Opt-blocker based implementation of value barriers */
-/* TODO: proof */
 static MLD_INLINE int64_t mld_value_barrier_i64(int64_t b)
 __contract__(ensures(return_value == b)) { return (b ^ mld_ct_get_optblocker_i64()); }
-/* TODO: proof */
+
 static MLD_INLINE uint32_t mld_value_barrier_u32(uint32_t b)
 __contract__(ensures(return_value == b)) { return (b ^ mld_ct_get_optblocker_u32()); }
 
@@ -67,6 +65,18 @@ __contract__(ensures(return_value == b))
   return b;
 }
 #endif /* MLD_USE_ASM_VALUE_BARRIER */
+
+
+/*
+ * The mld_ct_sel_int32 function below makes deliberate use of unsigned
+ * to signed integer conversion, which is implementation-defined
+ * behaviour. Here, we assume that uint16_t -> int16_t is inverse
+ * to int16_t -> uint16_t.
+ */
+#ifdef CBMC
+#pragma CPROVER check push
+#pragma CPROVER check disable "conversion"
+#endif
 
 /*************************************************
  * Name:        mld_ct_sel_int32
@@ -93,6 +103,23 @@ __contract__(
   return (int32_t)res;
 }
 
+/* Put unsigned-to-signed warnings in CBMC back into scope */
+#ifdef CBMC
+#pragma CPROVER check pop
+#endif
+
+
+
+/*
+ * The mld_ct_cmask_neg_i32 function below makes deliberate use of
+ * signed to unsigned integer conversion, which is fully defined
+ * behaviour in C. It is thus safe to disable this warning.
+ */
+#ifdef CBMC
+#pragma CPROVER check push
+#pragma CPROVER check disable "conversion"
+#endif
+
 
 /*************************************************
  * Name:        mld_ct_cmask_neg_i32
@@ -111,6 +138,11 @@ __contract__(ensures(return_value == ((x < 0) ? 0xFFFFFFFF : 0)))
   return (uint32_t)tmp;
 }
 
+/* Put signed-to-unsigned warnings in CBMC back into scope */
+#ifdef CBMC
+#pragma CPROVER check pop
+#endif
+
 /*************************************************
  * Name:        mld_ct_abs_i32
  *
@@ -121,7 +153,10 @@ __contract__(ensures(return_value == ((x < 0) ? 0xFFFFFFFF : 0)))
  **************************************************/
 /* TODO: proof */
 static MLD_INLINE uint32_t mld_ct_abs_i32(int32_t x)
-__contract__(ensures(return_value == ((x < 0) ? -x : x)))
+__contract__(
+  requires(x >= -INT32_MAX)
+  ensures(return_value == ((x < 0) ? -x : x))
+)
 {
   return mld_ct_sel_int32(-x, x, mld_ct_cmask_neg_i32(x));
 }
