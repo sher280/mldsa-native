28d664954b058ef1cb3cd02491e6c85f1a1ed1ca
sher280 <wardeniitrjawahar@gmail.com>
2025-08-13 17:45:07 +0530

    Update main.c
    Signed-off-by: sher280 <wardeniitrjawahar@gmail.com>

--- test/hal/main.c
+++ test/hal/main.c
@@ -1,1 +1,24 @@
+#include <stdio.h>
+#include <stdint.h>
 
+void enable_cyclecounter(void);
+void disable_cyclecounter(void);
+uint64_t get_cyclecounter(void);
+
+int main() {
+    enable_cyclecounter();
+    uint64_t start = get_cyclecounter();
+
+    
+    for (volatile int i = 0; i < 1000000; i++);
+
+    uint64_t end = get_cyclecounter();
+    disable_cyclecounter();
+
+    printf("Cycle Count: %llu\n", (unsigned long long)(end - start));
+    return 0;
+}
+
+// For linux
+//run: gcc -DPMU_CYCLES hal.c main.c -o test
+// ./test
